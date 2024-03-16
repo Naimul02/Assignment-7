845e5d3af43aea836c107ab02c1f4d1511ff343c
Naimul02 <naimulislum39@gmail.com>
2024-03-16 11:51:46 +0600

    added want to cook

--- src/components/Cooks/Cooks.jsx
+++ src/components/Cooks/Cooks.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Cooks = ({ selectedRecipes }) => {
+  console.log(selectedRecipes);
   return (
     <div className="border-2 p-10 lg:w-1/3 rounded-xl">
       <h2 className="text-2xl font-semibold text-center">Want to cook : </h2>
-      {/* table */}
-      
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedRecipes.map((selectedRecipe, idx) => {
+              return (
+                <tr key={idx}>
+                  <th>{idx + 1}</th>
+                  <td>{selectedRecipe.recipe_name}</td>
+                  <td>{selectedRecipe.preparing_time}</td>
+                  <td>{selectedRecipe.calories}</td>
+                  <td>
+                    <button className="btn bg-green-400 hover:text-black text-white">
+                      Preparing
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
